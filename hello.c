8ea41bb56f52c6094d1b380e944abf1fb409346d
dominator02 <55008447+dominator02@users.noreply.github.com>
2019-10-15 08:10:02 +0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include<stduio.h>
+#include<curl/curl.h>
+
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+  curl=curl_easy_init();
+  if(curl)
+  {
+    curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
+    res=curl_easy_perform(curl);
+    if(res!=CURLE_OK)
+      fprint(stderr,"curl_easy_perform() failed:%s\n",
+        curl_easy_strerror(curl);
+      curl_easy_cleanup(curl);
+  }
+  return 0;
+}
